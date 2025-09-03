557cb725297659b078b5cc7bbc349e6799896952
avni955587 <avni955587@gmail.com>
2025-09-03 13:29:31 +0530

    java

---
+++ STRINGJ/Strings.java
@@ -0,0 +1,22 @@
+package STRINGJ;
+import java.util.*;
+
+public class Strings {
+    public static void main(String[] args) {
+        
+        String name="Avni";
+        String fullname="AvniGangwar";
+
+        System.out.println(name);
+        System.out.println(fullname);
+
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter the string :  ");
+        String sentence=sc.nextLine();
+        System.out.println(sentence);
+        
+        sc.close();
+        
+    }
+    
+}
---
+++ STRINGJ/compare.java
@@ -0,0 +1,16 @@
+package STRINGJ;
+
+public class compare {  
+    public static void main(String[] args) {
+        String n1="AVNI";
+        String n2="Avni";
+        
+
+        if (n1.compareTo(n2)==0)
+        {
+ System.out.println("Strings are Equal");
+        }
+        else System.out.println("Not Equal");
+    }
+    
+}
